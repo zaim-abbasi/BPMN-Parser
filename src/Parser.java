d5e72f0037390bab1d82f06d69c5afb706220e47
Zaim Abbasi <zaim.k.abbasi@gmail.com>
2024-05-05 15:11:08 +0500

    Working on CT

---
+++ src/Parser.java
@@ -0,0 +1,286 @@
+import java.io.File;
+import javax.xml.parsers.*;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Parser {
+
+  public static void main(String[] args) {
+    String filename;
+    System.out.println("");
+    System.out.print("Enter file name: ");
+    Scanner sc = new Scanner(System.in);
+    filename = sc.nextLine();
+
+    String bpmnFilePath = "resource/" + filename;
+    Parser obj = new Parser();
+
+    obj.countBPMNElements(bpmnFilePath);
+    sc.close();
+
+    obj.addActivityTime(bpmnFilePath);
+
+    bpmnFilePath = "resource/updated_" + filename;
+    double cycleTime = obj.calculateCT(bpmnFilePath);
+    System.out.println("Cycle Time: " + cycleTime + " minutes");
+  }
+
+  public void countBPMNElements(String bpmnFilePath) {
+    File bpmnFile = new File(bpmnFilePath);
+    if (!bpmnFile.exists()) {
+      System.out.println("File not found: " + bpmnFilePath);
+      return;
+    }
+    try {
+      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+      Document doc = dbFactory.newDocumentBuilder().parse(bpmnFile);
+
+      XPathFactory xPathfactory = XPathFactory.newInstance();
+      XPath xpath = xPathfactory.newXPath();
+
+      // XPath expressions for counting BPMN elements
+      XPathExpression eventExpr = xpath.compile(
+          "count(//*[local-name()='startEvent' or local-name()='intermediateCatchEvent' or local-name()='intermediateThrowEvent' or local-name()='endEvent'])");
+      XPathExpression startEventExpr = xpath.compile("count(//*[local-name()='startEvent'])");
+      XPathExpression intermediateEventExpr = xpath
+          .compile("count(//*[local-name()='intermediateCatchEvent' or local-name()='intermediateThrowEvent'])");
+      XPathExpression endEventExpr = xpath.compile("count(//*[local-name()='endEvent'])");
+      XPathExpression taskExpr = xpath.compile("count(//*[local-name()='task'])");
+      XPathExpression userTaskExpr = xpath.compile("count(//*[local-name()='userTask'])");
+      XPathExpression serviceTaskExpr = xpath.compile("count(//*[local-name()='serviceTask'])");
+      XPathExpression scriptTaskExpr = xpath.compile("count(//*[local-name()='scriptTask'])");
+      XPathExpression manualTaskExpr = xpath.compile("count(//*[local-name()='manualTask'])");
+      XPathExpression subProcessExpr = xpath.compile("count(//*[local-name()='subProcess'])");
+      XPathExpression gatewayExpr = xpath.compile(
+          "count(//*[local-name()='exclusiveGateway' or local-name()='parallelGateway' or local-name()='inclusiveGateway'])");
+      XPathExpression exclusiveGatewayExpr = xpath.compile("count(//*[local-name()='exclusiveGateway'])");
+      XPathExpression parallelGatewayExpr = xpath.compile("count(//*[local-name()='parallelGateway'])");
+      XPathExpression inclusiveGatewayExpr = xpath.compile("count(//*[local-name()='inclusiveGateway'])");
+      XPathExpression artifactExpr = xpath
+          .compile("count(//*[local-name()='dataObject' or local-name()='group' or local-name()='textAnnotation'])");
+      XPathExpression dataObjectExpr = xpath.compile("count(//*[local-name()='dataObject'])");
+      XPathExpression groupExpr = xpath.compile("count(//*[local-name()='group'])");
+      XPathExpression annotationExpr = xpath.compile("count(//*[local-name()='textAnnotation'])");
+      XPathExpression connectingObjectsExpr = xpath.compile(
+          "count(//*[local-name()='sequenceFlow' or local-name()='messageFlow' or local-name()='association'])");
+      XPathExpression sequenceFlowExpr = xpath.compile("count(//*[local-name()='sequenceFlow'])");
+      XPathExpression messageFlowExpr = xpath.compile("count(//*[local-name()='messageFlow'])");
+      XPathExpression associationExpr = xpath.compile("count(//*[local-name()='association'])");
+      XPathExpression swimlaneExpr = xpath.compile("count(//*[local-name()='lane' or local-name()='pool'])");
+      XPathExpression poolExpr = xpath.compile("count(//*[local-name()='participant'])");
+
+      XPathExpression laneExpr = xpath.compile("count(//*[local-name()='lane'])");
+
+      // Execute XPath expressions
+      double totalEvents = (double) eventExpr.evaluate(doc, XPathConstants.NUMBER);
+      double startEvents = (double) startEventExpr.evaluate(doc, XPathConstants.NUMBER);
+      double intermediateEvents = (double) intermediateEventExpr.evaluate(doc, XPathConstants.NUMBER);
+      double endEvents = (double) endEventExpr.evaluate(doc, XPathConstants.NUMBER);
+      double userTasks = (double) userTaskExpr.evaluate(doc, XPathConstants.NUMBER);
+      double serviceTasks = (double) serviceTaskExpr.evaluate(doc, XPathConstants.NUMBER);
+      double scriptTasks = (double) scriptTaskExpr.evaluate(doc, XPathConstants.NUMBER);
+      double manualTasks = (double) manualTaskExpr.evaluate(doc, XPathConstants.NUMBER);
+      double tasks = userTasks + serviceTasks + scriptTasks + manualTasks; // Total tasks
+      double subProcesses = (double) subProcessExpr.evaluate(doc, XPathConstants.NUMBER);
+      double totalActivities = tasks + subProcesses; // Total activities
+      double totalGateways = (double) gatewayExpr.evaluate(doc, XPathConstants.NUMBER);
+      double exclusiveGateways = (double) exclusiveGatewayExpr.evaluate(doc, XPathConstants.NUMBER);
+      double parallelGateways = (double) parallelGatewayExpr.evaluate(doc, XPathConstants.NUMBER);
+      double inclusiveGateways = (double) inclusiveGatewayExpr.evaluate(doc, XPathConstants.NUMBER);
+      double totalArtifacts = (double) artifactExpr.evaluate(doc, XPathConstants.NUMBER);
+      double dataObjects = (double) dataObjectExpr.evaluate(doc, XPathConstants.NUMBER);
+      double groups = (double) groupExpr.evaluate(doc, XPathConstants.NUMBER);
+      double annotations = (double) annotationExpr.evaluate(doc, XPathConstants.NUMBER);
+      double totalConnectingObjects = (double) connectingObjectsExpr.evaluate(doc, XPathConstants.NUMBER);
+      double sequenceFlows = (double) sequenceFlowExpr.evaluate(doc, XPathConstants.NUMBER);
+      double messageFlows = (double) messageFlowExpr.evaluate(doc, XPathConstants.NUMBER);
+      double associations = (double) associationExpr.evaluate(doc, XPathConstants.NUMBER);
+      double totalSwimlanes = (double) swimlaneExpr.evaluate(doc, XPathConstants.NUMBER);
+      double pools = (double) poolExpr.evaluate(doc, XPathConstants.NUMBER);
+      double lanes = (double) laneExpr.evaluate(doc, XPathConstants.NUMBER);
+
+      // display the results
+      System.out.println();
+      System.out.println("BPMN Model Elements:");
+      System.out.println("--------------------------------");
+      System.out.println("   Total_Events: " + (int) totalEvents);
+      System.out.println("      Start_Events: " + (int) startEvents);
+      System.out.println("      Intermediate_Events: " + (int) intermediateEvents);
+      System.out.println("      End_Events: " + (int) endEvents);
+      System.out.println("--------------------------------");
+      System.out.println("   Total_Activities: " + (int) totalActivities);
+      System.out.println("      Tasks: " + (int) tasks);
+      System.out.println("        User_Tasks: " + (int) userTasks);
+      System.out.println("        Service_Tasks: " + (int) serviceTasks);
+      System.out.println("        Script_Tasks: " + (int) scriptTasks);
+      System.out.println("        Manual_Tasks: " + (int) manualTasks);
+      System.out.println("      Sub_Processes: " + (int) subProcesses);
+      System.out.println("--------------------------------");
+      System.out.println("   Total_Gateways: " + (int) totalGateways);
+      System.out.println("      Exclusive_Gateways_XOR: " + (int) exclusiveGateways);
+      System.out.println("      Parallel_Gateways_AND: " + (int) parallelGateways);
+      System.out.println("      Inclusive_Gateways_OR: " + (int) inclusiveGateways);
+      System.out.println("--------------------------------");
+      System.out.println("   Total_Artifacts: " + (int) totalArtifacts);
+      System.out.println("      Data_Objects: " + (int) dataObjects);
+      System.out.println("      Groups: " + (int) groups);
+      System.out.println("      Annotations: " + (int) annotations);
+      System.out.println("--------------------------------");
+      System.out.println("   Total_Connecting_Objects: " + (int) totalConnectingObjects);
+      System.out.println("      Sequence_Flows: " + (int) sequenceFlows);
+      System.out.println("      Message_Flows: " + (int) messageFlows);
+      System.out.println("      Associations: " + (int) associations);
+      System.out.println("--------------------------------");
+      System.out.println("   Total_Swimlanes: " + (int) totalSwimlanes);
+      System.out.println("      Pools: " + (int) pools);
+      System.out.println("      Lanes: " + (int) lanes);
+      System.out.println("--------------------------------");
+      System.out.println();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void addActivityTime(String bpmnFilePath) {
+    try {
+      File bpmnFile = new File(bpmnFilePath);
+      if (!bpmnFile.exists()) {
+        System.out.println("File not found: " + bpmnFilePath);
+        return;
+      }
+
+      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+      Document doc = dBuilder.parse(bpmnFile);
+
+      Random rand = new Random();
+      NodeList taskList = doc.getElementsByTagName("task");
+      for (int i = 0; i < taskList.getLength(); i++) {
+        Node taskNode = taskList.item(i);
+        if (taskNode.getNodeType() == Node.ELEMENT_NODE) {
+          Element taskElement = (Element) taskNode;
+          int duration = rand.nextInt(11) + 5; // Random duration between 5 to 15 minutes
+          taskElement.setAttribute("duration", String.valueOf(duration));
+        }
+      }
+
+      // Write the updated XML to a new file
+      String updatedFilename = "updated_" + bpmnFile.getName();
+      TransformerFactory transformerFactory = TransformerFactory.newInstance();
+      Transformer transformer = transformerFactory.newTransformer();
+      DOMSource source = new DOMSource(doc);
+      StreamResult result = new StreamResult(new File("resource/" + updatedFilename));
+      transformer.transform(source, result);
+
+      System.out.println("Activity times added and saved to: " + updatedFilename);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public double calculateCT(String bpmnFilePath) {
+    double cycleTime = 0.0;
+    try {
+      File bpmnFile = new File(bpmnFilePath);
+      if (!bpmnFile.exists()) {
+        System.out.println("File not found: " + bpmnFilePath);
+        return cycleTime;
+      }
+
+      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+      Document doc = dBuilder.parse(bpmnFile);
+
+      XPathFactory xPathfactory = XPathFactory.newInstance();
+      XPath xpath = xPathfactory.newXPath();
+
+      // XPath expression to sum up task durations
+      XPathExpression taskDurationExpr = xpath.compile("//task/@duration");
+
+      NodeList durationNodes = (NodeList) taskDurationExpr.evaluate(doc, XPathConstants.NODESET);
+      for (int i = 0; i < durationNodes.getLength(); i++) {
+        Node durationNode = durationNodes.item(i);
+        int duration = Integer.parseInt(durationNode.getNodeValue());
+        cycleTime += duration;
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return cycleTime;
+  }
+
+  // public double calculateCTT(String bpmnFilePath) {
+  // double cycleTime = 0.0;
+  // try {
+  // File bpmnFile = new File(bpmnFilePath);
+  // if (!bpmnFile.exists()) {
+  // System.out.println("File not found: " + bpmnFilePath);
+  // return cycleTime;
+  // }
+
+  // DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+  // DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+  // Document doc = dBuilder.parse(bpmnFile);
+
+  // XPathFactory xPathfactory = XPathFactory.newInstance();
+  // XPath xpath = xPathfactory.newXPath();
+
+  // // XPath expression to sum up task durations
+  // XPathExpression taskDurationExpr = xpath.compile("//task/@duration");
+
+  // NodeList durationNodes = (NodeList) taskDurationExpr.evaluate(doc,
+  // XPathConstants.NODESET);
+  // for (int i = 0; i < durationNodes.getLength(); i++) {
+  // Node durationNode = durationNodes.item(i);
+  // int duration = Integer.parseInt(durationNode.getNodeValue());
+  // cycleTime += duration;
+  // }
+
+  // // Handle exclusive gateways
+  // XPathExpression gatewayExpr = xpath.compile("//exclusiveGateway");
+  // NodeList gatewayNodes = (NodeList) gatewayExpr.evaluate(doc,
+  // XPathConstants.NODESET);
+  // for (int i = 0; i < gatewayNodes.getLength(); i++) {
+  // Node gatewayNode = gatewayNodes.item(i);
+  // if (gatewayNode.getNodeType() == Node.ELEMENT_NODE) {
+  // Element gatewayElement = (Element) gatewayNode;
+  // NodeList outgoingNodes = gatewayElement.getElementsByTagName("outgoing");
+  // double maxPathTime = 0.0;
+  // for (int j = 0; j < outgoingNodes.getLength(); j++) {
+  // Node outgoingNode = outgoingNodes.item(j);
+  // String sequenceFlowId = outgoingNode.getTextContent();
+  // XPathExpression pathExpr = xpath
+  // .compile("//sequenceFlow[@id='" + sequenceFlowId +
+  // "']/following-sibling::task/@duration");
+  // Node pathNode = (Node) pathExpr.evaluate(doc, XPathConstants.NODE);
+  // if (pathNode != null) {
+  // double pathTime = Double.parseDouble(pathNode.getNodeValue());
+  // if (pathTime > maxPathTime) {
+  // maxPathTime = pathTime;
+  // }
+  // }
+  // }
+  // cycleTime += maxPathTime;
+  // }
+  // }
+
+  // } catch (Exception e) {
+  // e.printStackTrace();
+  // }
+  // return cycleTime;
+  // }
+}
